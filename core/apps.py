b1d2613fda67f8aebf34bfe77a716f6ab817445a
Diego <diegoospina1993@gmail.com>
2025-08-06 22:29:50 -0500

    Se realiza reestructuración del proyecto base

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProyectosConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proyectos'
+    name = 'core'
