e5b849609f8bb90a85d70d58b704a5cb0cb6e82e
Diego <diegoospina1993@gmail.com>
2025-11-10 22:40:15 -0500

    creación y configuración del archivo gitignore

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -118,10 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-    
-]
+STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
+
+
+#Archivos Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/models.py
+++ core/models.py
@@ -1,52 +1,63 @@
 from django.db import models
 
-# Create your models here.
+# Definición de modelos
 
-class about(models.Model):
-    nombre = models.CharField(max_length=100)
-    profesion = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    foto = models.ImageField(upload_to='sobre_mi/', blank=True, null=True)
+class Profile(models.Model):
+    
+    #Definicion de campos del modelo profile
+    full_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100, verbose_name="Profesional")
+    biography = models.TextField(verbose_name="Biography")
+    profile_image = models.ImageField(upload_to='profile/', blank=True, null=True)
+    resume = models.FileField(upload_to='resume/', blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     #cv = acá va la hoja de vida
     
     def __str__(self):
-        return self.nombre
+        return self.full_name
     
     
-class Curso(models.Model):
-    titulo = models.CharField(max_length=200)
-    institucion = models.CharField(max_length=200)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField(blank=True, null=True)
-    descripcion = models.TextField(blank=True)
+class Project(models.Model):
+    #Definición campos del modelo project
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    image = models.ImageField(upload_to='proyectos/', blank=True, null=True, verbose_name="Image")
+    link = models.URLField(blank=True, null= True, verbose_name="Project Link")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     
-    def __str__(self):
-        return self.titulo
-    
-class Proyecto(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='proyectos/', blank=True, null=True)
-    link = models.URLField(blank=True, null= True)
+    class Meta:
+        ordering = ['-created_at']
+        
     
     def __str__(self):
-        return self.titulo
+        return self.title
+
+
+class Certification(models.Model):
+    title = models.CharField(max_length=200)
+    institution = models.CharField(max_length=200)
+    date_obtained = models.DateField()
+    certificate_file = models.FileField(upload_to='certifications/', blank=True, null=True)
     
-class ExperienciaLaboral(models.Model):
-    puesto = models.CharField(max_length=200)
-    empresa = models.CharField(max_length=200)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField(blank=True, null=True)
-    descripcion = models.TextField(blank=True)
+    class Meta:
+        ordering = ['-date_obtained']
     
     def __str__(self):
-        return f"{self.puesto} en {self.empresa}"
-    
-class MensajeContacto(models.Model):
-    nombre = models.CharField(max_length=100)
+        return f"{self.title} - {self.institution}"
+
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField()
-    mensaje = models.TextField()
-    fecha_envio = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['-created_at']
     
     def __str__(self):
-        return f"Mensaje de {self.nombre} ({self.email})"
+        return f"Mensaje de {self.name}"
